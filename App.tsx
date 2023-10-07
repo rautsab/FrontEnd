0cea38a7a89c3d4f5a7d7bba436394ab8d1cd264
rautsab <rautsab@11gmail.com>
2023-10-07 10:40:09 +0600

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <Text style={styles.textContainer}>Hello World</Text>
+      <Text style={styles.newContainer}>This is Redwan</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
+  textContainer: {
+    fontWeight: "700",
+    fontSize: 16,
+    color: "red"
+  },
+  newContainer: {
+    fontWeight: "900",
+    fonts: 26,
+    color: "black"
+  }
 });
+
+export default App;
